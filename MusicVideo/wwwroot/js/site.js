7c164bec5df08cf01650671b350cd22ef818fca2
npranno <77467876+npranno@users.noreply.github.com>
2021-06-29 09:34:37 -0700

    Add project files.

---
+++ MusicVideo/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+var id = null;
+function motion() {
+    var elem = document.getElementById("animation");
+    var pos = 0
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 500) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.left = pos + 'px';
+        }
+    }
+}
